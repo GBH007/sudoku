c011aa0943fd26ac737d4df76c50e7e6c9282452
Никониров Григорий <mrgbh007@gmail.com>
2018-05-16 11:22:30 +0700

    добавление структуры данных для судоку

---
+++ sudoku.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"strconv"
+)
+
+type Sudoku struct {
+	field [9][9]int
+}
+
+func (su *Sudoku) GetHashStr() string {
+	return su.print("", "")
+}
+func (su *Sudoku) print(sep, end string) string {
+	buff := new(bytes.Buffer)
+	for i := 0; i < 9; i++ {
+		if i != 0 {
+			buff.WriteString(end)
+		}
+		for j := 0; j < 9; j++ {
+			if j != 0 {
+				buff.WriteString(sep)
+			}
+			buff.WriteString(strconv.Itoa(su.field[i][j]))
+		}
+	}
+	return buff.String()
+}
+func (su *Sudoku) LoadFromHashStr(s string) {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			//n, _ := strconv.Atoi(s[i*9+j : i*9+j+1])
+			n := int(s[i*9+j]) - 48
+			su.field[i][j] = n
+		}
+	}
+}
+func (su *Sudoku) String() string {
+	return su.print(" ", "\n")
+}
---
+++ sudoku_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHash(t *testing.T) {
+	in := "000040700031500006600037090000093025000000000950680000080310009400008630003060000"
+	su := new(Sudoku)
+	su.LoadFromHashStr(in)
+	out := su.GetHashStr()
+	if in != out {
+		t.Error("in != out")
+	}
+}
+func BenchmarkLoad(b *testing.B) {
+	in := "000040700031500006600037090000093025000000000950680000080310009400008630003060000"
+	su := new(Sudoku)
+	for i := 0; i < b.N; i++ {
+		su.LoadFromHashStr(in)
+	}
+}
