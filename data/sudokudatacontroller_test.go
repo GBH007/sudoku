cb131bd19fc7705ae4e0dff84a10dd44e4ef8aa5
Никониров Григорий <mrgbh007@gmail.com>
2018-05-16 14:45:27 +0700

    добавление тестов для работы с патчами

--- data/sudokudatacontroller_test.go
+++ data/sudokudatacontroller_test.go
@@ -5,6 +5,13 @@ import (
 	"testing"
 )
 
+type testPatch struct {
+	row, col, num int
+}
+
+func (t testPatch) Row() int    { return t.row }
+func (t testPatch) Column() int { return t.col }
+func (t testPatch) Number() int { return t.num }
 func BenchmarkSetUnset(b *testing.B) {
 	su := new(Sudoku)
 	su.LoadFromHashStr(strings.Repeat("0", 81))
@@ -13,5 +20,16 @@ func BenchmarkSetUnset(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		sdc.set(0, 0, 1)
 		sdc.unset(0, 0, 1)
+	}
+}
+func BenchmarkSetUnsetPatch(b *testing.B) {
+	su := new(Sudoku)
+	su.LoadFromHashStr(strings.Repeat("0", 81))
+	sdc := NewCacheController(su)
+	patch := testPatch{0, 0, 1}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sdc.Set(patch)
+		sdc.Unset(patch)
 	}
 }
