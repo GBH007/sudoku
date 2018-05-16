ae79f8eeb7e52f92ee15b3b33eea365cd4a57599
Никониров Григорий <mrgbh007@gmail.com>
2018-05-16 09:43:41 +0700

    первичная подготовка файлов

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# author:   Григорий Никониров
-#           Gregoriy Nikonirov
-# email:    mrgbh007@gmail.com
-#
-
-from .strategy import Controller
-from .sudoku import SudokuData
-__all__=['Controller','SudokuData']
